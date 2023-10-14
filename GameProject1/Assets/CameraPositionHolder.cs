62bf40dfbfc1cadfc3ac4aef02c38d7b1a91cd0c
Max Phillips <11034905@uvu.edu>
2023-10-13 20:28:43 -0600

    Work on Unity

---
+++ GameProject1/Assets/CameraPositionHolder.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPositionHolder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform cameraPosition;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cameraPosition.position;
+    }
+}
--- GameProject1/Assets/PlayerCharacter.cs
+++ GameProject1/Assets/PlayerCharacter.cs
@@ -6,22 +6,9 @@ public class PlayerCharacter : MonoBehaviour
 {
     private Rigidbody characterController;
 
-    public float Speed = 1f;
-    // Start is called before the first frame update
-
-    public float sensX;
-    public float sensY;
-
-    public Transform orientation;
-
-    float xRotation;
-    float yRotation;
-
-    void Start()
     {
         characterController = GetComponent<Rigidbody>();
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+       
     }
 
     // Update is called once per frame
@@ -38,16 +25,6 @@ public class PlayerCharacter : MonoBehaviour
 
         if(Input.GetKey(KeyCode.A))
             transform.Translate(Vector3.right * -Speed * Time.deltaTime);
-
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
-
-        yRotation += mouseX;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
         
     }
 }
