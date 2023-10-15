50700c61c1c016d1dc596cf2ed894274993205cc
Max Phillips <11034905@uvu.edu>
2023-10-15 11:39:41 -0600

    Created lasers and added two scripts for them.

---
+++ UFO defense force Prototype game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 25;
+
+    public float xRange = 30;
+
+    public Transform blaster;
+    public GameObject laserBolt;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get's input of horizontal keys (left/right and 'a' and 'd')
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //Moves player left and right.
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //keeping the player in bounds
+        //Left side
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        //if spacebar is pressed, fire laserbolt
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Create the laserBolt at the blasters position, maintaining the Objects Rotation.
+            Instantiate(laserBolt, blaster.transform.position, laserBolt.transform.rotation);
+        }
+    }
+}
