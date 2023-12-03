c0e2cf94d7112204265a51efe1a8d0e50ab4549e
Max Phillips <11034905@uvu.edu>
2023-12-03 15:09:16 -0700

    put all the needed 5 and 6 scripts in one folder

---
+++ My project/Assets/Unit5and6/GameObjectDisabler.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GameObjectDisabler : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        var triggerEventsBehaviour = FindObjectOfType<TriggerEventsBehaviour>();
+        if (triggerEventsBehaviour != null)
+        {
+            triggerEventsBehaviour.triggerExitEvent.AddListener(DisableGameObject);
+        }
+    }
+
+    private void OnDisable()
+    {
+        var triggerEventsBehaviour = FindObjectOfType<TriggerEventsBehaviour>();
+        if (triggerEventsBehaviour != null)
+        {
+            triggerEventsBehaviour.triggerExitEvent.RemoveListener(DisableGameObject);
+        }
+    }
+
+    private void DisableGameObject()
+    {
+        gameObject.SetActive(false);
+    }
+}
